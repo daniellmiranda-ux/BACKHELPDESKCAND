febacec5d6ed23a94c1854c9b98b157e4af5eb96
daniellmiranda-ux <daniel.l.miranda@ba.estudante.senai.br>
2026-08-18 21:28:48 -0300

    Add files via upload

---
+++ backhelp/backhelp/src/main/java/com/example/backhelp/model/Categoria.java
@@ -0,0 +1,7 @@
+package com.example.backhelp.model;
+
+public enum Categoria {
+
+    HARDWARE,
+    SOFTWARE
+}
---
+++ backhelp/backhelp/src/main/java/com/example/backhelp/model/StatusChamado.java
@@ -0,0 +1,10 @@
+package com.example.backhelp.model;
+
+public enum StatusChamado {
+
+    ABERTO,
+    EM_ATENDIMENTO,
+    ATRASADO,
+    CONCLUIDO,
+    FECHADO
+}
